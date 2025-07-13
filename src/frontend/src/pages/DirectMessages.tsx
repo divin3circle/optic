d4ed03342f326cdafb8ad39fb6f1083c66a9e3e9
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2025-07-13 15:22:53 +0300

    Refactor DirectMessages component for improved responsiveness and user experience
    - Updated layout logic in the DirectMessages component to enhance responsiveness on mobile devices.
    - Integrated motion animations for a smoother user interface when no chat is selected.
    - Added a logo and messaging prompt for better user engagement when no active chat is available.

--- src/frontend/src/pages/DirectMessages.tsx
+++ src/frontend/src/pages/DirectMessages.tsx
@@ -3,31 +3,57 @@ import React from "react";
 import ChatSidebar from "../components/app/chats/ChatSidebar";
 import ChatWindow from "../components/app/chats/ChatWindow";
 import { cn } from "@/lib/utils";
+import { motion } from "framer-motion";
+import logo from "../../assets/images/icon.png";
 
 type selectedChatId = string | null;
 
-const selectedChatIdMock: selectedChatId = "1";
-
 function DirectMessages() {
   const isMobile = useChatIsMobile();
+
+  const selectedChatIdMock: selectedChatId = "1";
+
   return (
-    <div className="flex h-screen w-full ">
-      <div
-        className={cn(
-          "w-full lg:w-[40%]",
-          isMobile && selectedChatIdMock ? "hidden" : "block"
-        )}
-      >
-        <ChatSidebar />
-      </div>
-      <div
-        className={cn(
-          "w-full lg:w-[60%]",
-          isMobile && !selectedChatIdMock ? "hidden" : "block"
-        )}
-      >
-        <ChatWindow />
-      </div>
+    <div className="flex h-screen w-full">
+      {(!isMobile || !selectedChatIdMock) && (
+        <div className={cn("w-full lg:w-[40%]")}>
+          <ChatSidebar />
+        </div>
+      )}
+
+      {(!isMobile || selectedChatIdMock) && (
+        <div className={cn("w-full lg:w-[60%]")}>
+          {selectedChatIdMock ? (
+            <ChatWindow />
+          ) : (
+            <div className="flex items-center justify-center flex-col h-full">
+              <a>
+                <motion.div
+                  initial={{ opacity: 0, y: 100 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 0.5 }}
+                  exit={{ opacity: 0, y: 100 }}
+                  className="flex items-center justify-center gap-2  p-2 rounded-3xl cursor-pointer"
+                >
+                  <img src={logo} alt="logo" className="w-10 h-10" />
+                  <h1 className="text-2xl font-karla-bold text-primary">
+                    Optic
+                  </h1>
+                </motion.div>
+              </a>
+              <motion.h1
+                initial={{ opacity: 0, y: 100 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5 }}
+                exit={{ opacity: 0, y: 100 }}
+                className="text-gray-500 font-karla text-lg"
+              >
+                Select a chat to start messaging
+              </motion.h1>
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
 }
